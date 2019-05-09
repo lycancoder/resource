205ad9af9e59423f9d45d0b10c90fe10cc925ee3
lycan <40607461+lycancoder@users.noreply.github.com>
2019-05-09 13:03:31 +0800

    Add files via upload

--- js定时器/timers.js
+++ js定时器/timers.js
@@ -2,22 +2,22 @@
 var timer;
 var showTime;
 function createTimer(sec, callback) {
-	sec = isNaN(sec) ? 0 : parseInt(sec);
+    sec = isNaN(sec) ? 0 : parseInt(sec);
 
-	timer = setInterval(function () {
-		var m = parseInt(sec / 60);
-		var s = parseInt(sec % 60);
-		showTime = toDub(m) + ':' + toDub(s);
+    timer = setInterval(function () {
+        var m = parseInt(sec / 60);
+        var s = parseInt(sec % 60);
+        showTime = toDub(m) + ':' + toDub(s);
 
-		callback(sec, showTime);
-		sec += 1;
-	}, 1000);
+        callback(sec, showTime);
+        sec += 1;
+    }, 1000);
 }
 
 function toDub(n) {
-	return n < 10 ? '0' + n : n;
+    return n < 10 ? '0' + n : n;
 }
 
 function clearTimer() {
-	clearInterval(timer);
+    clearInterval(timer);
 }
